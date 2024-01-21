15a68f06558505d5897afd49cde24e53e2d11224
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-21 11:58:43 -0600

    feat(AvaxGods.sol): implement beforeTokenTansfer contract function

--- web3/contracts/AvaxGods.sol
+++ web3/contracts/AvaxGods.sol
@@ -485,14 +485,14 @@ contract AVAXGods is ERC1155, Ownable, ERC1155Supply {
   }
 
   // The following functions are overrides required by Solidity.
-//   function _beforeTokenTransfer(
-//     address operator,
-//     address from,
-//     address to,
-//     uint256[] memory ids,
-//     uint256[] memory amounts,
-//     bytes memory data
-//   ) internal override(ERC1155, ERC1155Supply) {
-//     super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-//   }
+  function _beforeTokenTransfer(
+    address operator,
+    address from,
+    address to,
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) internal override(ERC1155, ERC1155Supply) {
+    super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+  }
 }
