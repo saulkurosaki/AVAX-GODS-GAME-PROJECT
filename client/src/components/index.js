246b706b4fb8699cadd09c22146f4f9d4d1f9925
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-22 17:24:41 -0600

    feat(Alert): implement complete Alert component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as PageHOC } from "./PageHOC";
 export { default as CustomInput } from "./CustomInput";
 export { default as CustomButton } from "./CustomButton";
+export { default as Alert } from "./Alert";
