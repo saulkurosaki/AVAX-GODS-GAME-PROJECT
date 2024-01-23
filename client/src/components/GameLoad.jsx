831383e13df3274ac88382c939eb9b85b11a418f
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-22 18:37:48 -0600

    feat(GameLoad): implement complete GameLoad component

--- client/src/components/GameLoad.jsx
+++ client/src/components/GameLoad.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import CustomButton from "./CustomButton";
+import { useGlobalContext } from "../context";
+import { player01, player02 } from "../assets";
+import styles from "../styles";
 
 const GameLoad = () => {
-  return <div>Game Load component Test c:</div>;
+  const { walletAddress } = useGlobalContext();
+  const navigate = useNavigate();
+
+  return (
+    <div className={`${styles.flexBetween} ${styles.gameLoadContainer}`}>
+      <div className={styles.gameLoadBtnBox}>
+        <CustomButton
+          title="Choose Battleground"
+          handleClick={() => navigate("/battleground")}
+          restStyles="mt-5"
+        />
+      </div>
+
+      <div className={`flex-1 ${styles.flexCenter} flex-col`}>
+        <h1 className={`${styles.headText} text-center`}>
+          Waiting for a <br /> worthy component...
+        </h1>
+        <p className={styles.gameLoadText}>
+          Protip: While you're waiting, choose your preferred battleground
+        </p>
+
+        <div className={styles.gameLoadPlayersBox}>
+          //Player01
+          <div className={`${styles.flexCenter} flex-col`}>
+            <img
+              src={player01}
+              alt="player01"
+              className={styles.gameLoadPlayerImg}
+            />
+            <p className={styles.gameLoadPlayerText}>
+              {walletAddress.slice(0, 30)}
+            </p>
+          </div>
+          <h2 className={styles.gameLoadVS}>Vs</h2>
+          //Unknown Player
+          <div className={`${styles.flexCenter} flex-col`}>
+            <img
+              src={player02}
+              alt="player02"
+              className={styles.gameLoadPlayerImg}
+            />
+            <p className={styles.gameLoadPlayerText}>????????????????</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GameLoad;
