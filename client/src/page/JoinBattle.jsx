fa476d267fe0b24823612d2f92f24490452e80db
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-24 12:34:54 -0600

    feat(JoinBattle): implement available battles display

--- client/src/page/JoinBattle.jsx
+++ client/src/page/JoinBattle.jsx
@@ -6,11 +6,42 @@ import { CustomButton, PageHOC } from "../components";
 import styles from "../styles";
 
 const JoinBattle = () => {
+  const {
+    contract,
+    gameData,
+    fetchGameData,
+    walletAddress,
+    setShowAlert,
+    setBattleName,
+  } = useGlobalContext();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchGameData(walletAddress);
+  }, [contract, walletAddress]);
 
   return (
     <>
       <h2 className={styles.joinHeadText}>Available Battles:</h2>
+
+      <div className={styles.joinContainer}>
+        {gameData.pendingBattles.length ? (
+          gameData.pendingBattles
+            .filter((battle) => !battle.players.includes(walletAddress))
+            .map((battle, index) => (
+              <div key={battle.name + index} className={styles.flexBetween}>
+                <p className={styles.joinBattleTitle}>
+                  {index + 1}. {battle.name}
+                </p>
+              </div>
+            ))
+        ) : (
+          <p className={styles.joinLoading}>
+            Reload the page to see new battles
+          </p>
+        )}
+      </div>
+
       <p className={styles.infoText} onClick={() => navigate("/create-battle")}>
         Or create a new battle
       </p>
