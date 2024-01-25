c0f25487e3009f47d70c60c08c3244124bd06dcf
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-25 12:16:03 -0600

    feat(Card): implement character attack damage display

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -18,6 +18,12 @@ const Card = ({ card, title, restStyles, cardRef, playerTwo }) => {
         alt="card"
         className={styles.cardImg}
       />
+
+      <div
+        className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}
+      >
+        <p className={`${styles.cardPoint} text-yellow-400`}>{card.att}</p>
+      </div>
     </div>
   );
 };
