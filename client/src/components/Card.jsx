38f83a5a3b2da8d00d4f2598769c0bd97b8d2f74
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-25 12:07:50 -0600

    feat(Card): implement generateRandomCardImage function

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
+
+import styles from "../styles";
+import { allCards } from "../assets";
+
+const generateRandomCardImage = () =>
+  allCards[Math.floor(Math.random() * allCards.length - 1)];
+
+const img1 = generateRandomCardImage();
+const img2 = generateRandomCardImage();
 
 const Card = () => {
   return <div>Card Component Test c:</div>;
