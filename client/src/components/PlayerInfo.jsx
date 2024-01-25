a17a1af0b5dc93bb29898936ca79d204fb4d7f2e
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-25 11:49:56 -0600

    feat(PlayerInfo): implement battle players health responsive bar

--- client/src/components/PlayerInfo.jsx
+++ client/src/components/PlayerInfo.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import ReactTooltip from "react-tooltip";
 
-const PlayerInfo = () => {
-  return <div>Player Info Component Test c:</div>;
+import styles from "../styles";
+
+const healthPoints = 25;
+
+const healthLevel = (points) =>
+  points >= 12 ? "bg-green-500" : points >= 6 ? "bg-orange-500" : "bg-red-500";
+
+const marginIndexing = (index) =>
+  index !== healthPoints - 1 ? "mr-1" : "mt-0";
+
+const PlayerInfo = ({ player, playerIcon, mt }) => {
+  return (
+    <div className={`${styles.flexCenter} ${mt ? "mt-4" : "mb-4"}`}>
+      <img
+        data-for={`Player-${mt ? "1" : "2"}`}
+        data-tip
+        src={playerIcon}
+        alt="player2"
+        className="w-14 h-14 object-contain rounded-full"
+      />
+
+      <div
+        data-for={`Health-${mt ? "1" : "2"}`}
+        data-tip={`Health: ${player?.health}`}
+        className={styles.playerHealth}
+      >
+        {[...Array(player.health).keys()].map((item, index) => (
+          <div
+            key={`player-item-${item}`}
+            className={`${styles.playerHealthBar} ${healthLevel(
+              player.health
+            )} ${marginIndexing(index)}`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PlayerInfo;
