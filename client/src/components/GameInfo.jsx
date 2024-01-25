a1d2f565e9e823874ca456d53f446e2916b12b9f
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-25 12:46:53 -0600

    feat(GameInfo): implement battle info sidebar icon

--- client/src/components/GameInfo.jsx
+++ client/src/components/GameInfo.jsx
@@ -7,7 +7,24 @@ import { alertIcon, gameRules } from "../assets";
 import styles from "../styles";
 
 const GameInfo = () => {
-  return <div>Game Info Component Test c:</div>;
+  const { contract, gameData, setShowAlert } = useGlobalContext();
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const navigate = useNavigate();
+
+  const handleBattleExit = async () => {};
+
+  return (
+    <>
+      <div className={styles.gameInfoIconBox}>
+        <div
+          className={`${styles.gameInfoIcon} ${styles.flexCenter}`}
+          onClick={() => setToggleSidebar(true)}
+        >
+          <img src={alertIcon} alt="info" className={styles.gameInfoIconImg} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default GameInfo;
