571443461fd3433564e8a9a320417c641333b2cf
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-25 12:39:30 -0600

    feat(GameInfo): implement all hooks and import for the GameInfo component

--- client/src/components/GameInfo.jsx
+++ client/src/components/GameInfo.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import CustomButton from "./CustomButton";
+import { useGlobalContext } from "../context";
+import { alertIcon, gameRules } from "../assets";
+import styles from "../styles";
 
 const GameInfo = () => {
   return <div>Game Info Component Test c:</div>;
