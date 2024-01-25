0c2036515a0b812dd1e980e2dcb3bead0e7ec6ed
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-25 13:04:20 -0600

    feat(Battleground): implement Battleground page component initial layout

---
+++ client/src/page/Battleground.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Battleground = () => {
+  return <div>Battleground Page Test c:</div>;
+};
+
+export default Battleground;
--- client/src/page/index.js
+++ client/src/page/index.js
@@ -2,3 +2,4 @@ export { default as Home } from "./Home";
 export { default as CreateBattle } from "./CreateBattle";
 export { default as JoinBattle } from "./JoinBattle";
 export { default as Battle } from "./Battle";
+export { default as Battleground } from "./Battleground";
