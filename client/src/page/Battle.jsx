0cdd23cc8e95f9b64c63f685aacb5bdd5da30542
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-25 11:34:03 -0600

    feat(Battle): implement complete Battle component

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import styles from "../styles";
-import { Alert } from "../components";
+import { Alert, ActionButton, Card, GameInfo, PlayerInfo } from "../components";
 import { useGlobalContext } from "../context";
 import {
   attack,
@@ -84,7 +84,40 @@ const Battle = () => {
     <div
       className={`${styles.flexBetween} ${styles.gameContainer} ${battleGround}`}
     >
-      <h1 className="text-xl">{battleName}</h1>
+      {showAlert?.status && (
+        <Alert type={showAlert.type} message={showAlert.message} />
+      )}
+
+      <PlayerInfo player={player2} playerIcon={player02Icon} mt />
+
+      <div className={`${styles.flexCenter} flex-col my-10`}>
+        <Card card={player2} title={player2?.playerName} cardRef="" playerTwo />
+
+        <div className="flex items-center flex-row">
+          <ActionButton
+            imgUrl={attack}
+            handleClick={() => {}}
+            restStyles="mr-2 hover:border-yellow-400"
+          />
+
+          <Card
+            card={player1}
+            title={player1?.playerName}
+            cardRef=""
+            restStyles="mt-3"
+          />
+
+          <ActionButton
+            imgUrl={defense}
+            handleClick={() => {}}
+            restStyles="ml-6 hover:border-red-600"
+          />
+        </div>
+      </div>
+
+      <PlayerInfo player={player1} playerIcon={player01Icon} mt />
+
+      <GameInfo />
     </div>
   );
 };
