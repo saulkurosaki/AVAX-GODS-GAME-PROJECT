9c5c6549732a8443ef7cbebb679a35adf5581c77
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-25 18:35:04 -0600

    feat(createEventListeners.js): implement the battleMoveEventFilter function

--- client/src/context/createEventListeners.js
+++ client/src/context/createEventListeners.js
@@ -48,4 +48,10 @@ export const createEventListeners = ({
 
     setUpdateGameData((prevUpdateGameData) => prevUpdateGameData + 1);
   });
+
+  const BattleMoveEventFilter = contract.filters.BattleMove();
+
+  AddNewEvent(BattleMoveEventFilter, provider, ({ args }) => {
+    console.log("Battle move initiated!", args);
+  });
 };
