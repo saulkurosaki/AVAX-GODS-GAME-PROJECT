71f20e09225e3a455e9392a8b595191b32754c9c
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-26 18:43:56 -0600

    feat(context/index.jsx): implement the refs for both of the two players

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -35,6 +35,9 @@ export const GlobalContextProvider = ({ children }) => {
   const [step, setStep] = useState(1);
   const [errorMessage, setErrorMessage] = useState("");
 
+  const player1Ref = useRef();
+  const player2Ref = useRef();
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -99,6 +102,8 @@ export const GlobalContextProvider = ({ children }) => {
         walletAddress,
         setShowAlert,
         setUpdateGameData,
+        player1Ref,
+        player2Ref,
       });
     }
   }, [contract, walletAddress, step]);
@@ -174,6 +179,8 @@ export const GlobalContextProvider = ({ children }) => {
         setBattleGround,
         errorMessage,
         setErrorMessage,
+        player1Ref,
+        player2Ref,
       }}
     >
       {children}
