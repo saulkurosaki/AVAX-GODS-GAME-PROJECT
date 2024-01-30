5e2ec8121848d6ce683edb113c155f89549d8c73
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-29 19:17:16 -0600

    feat(CreateBattle): implement gas limit config for the createBattle contract function call

--- client/src/page/CreateBattle.jsx
+++ client/src/page/CreateBattle.jsx
@@ -29,7 +29,9 @@ const CreateBattle = () => {
     if (!battleName || !battleName.trim()) return null;
 
     try {
-      await contract.createBattle(battleName);
+      await contract.createBattle(battleName, {
+        gasLimit: 200000,
+      });
 
       setWaitBattle(true);
     } catch (error) {
