b876aeeaff7363f70a1662599d559c154f6e5398
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-29 18:55:55 -0600

    feat(GameInfo): implement exit battle feature

--- client/src/components/GameInfo.jsx
+++ client/src/components/GameInfo.jsx
@@ -7,11 +7,26 @@ import { alertIcon, gameRules } from "../assets";
 import styles from "../styles";
 
 const GameInfo = () => {
-  const { contract, gameData, setShowAlert } = useGlobalContext();
+  const { contract, gameData, setShowAlert, setErrorMessage } =
+    useGlobalContext();
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const navigate = useNavigate();
 
-  const handleBattleExit = async () => {};
+  const handleBattleExit = async () => {
+    const battleName = gameData.activeBattle.name;
+
+    try {
+      await contract.quitBattle(battleName);
+
+      setShowAlert({
+        status: true,
+        type: "info",
+        message: `You're quitting the ${battleName}`,
+      });
+    } catch (error) {
+      setErrorMessage(error);
+    }
+  };
 
   return (
     <>
