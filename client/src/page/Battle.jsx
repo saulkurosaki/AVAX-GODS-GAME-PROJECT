14f023d701a21185312d82f1b4d52b873666e7bc
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-29 19:14:56 -0600

    feat(Battle): implement gas limit config for the attackOrDefentChoice contract function call

--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -87,7 +87,9 @@ const Battle = () => {
     playAudio(choice === 1 ? attackSound : defenseSound);
 
     try {
-      await contract.attackOrDefendChoice(choice, battleName);
+      await contract.attackOrDefendChoice(choice, battleName, {
+        gasLimit: 200000,
+      });
 
       setShowAlert({
         status: true,
