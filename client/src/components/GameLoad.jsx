b66524752aaa683fc8ebb3522fd4474f1cef764c
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-31 12:16:57 -0600

    feat(GameLoad): little component refactor

--- client/src/components/GameLoad.jsx
+++ client/src/components/GameLoad.jsx
@@ -16,40 +16,41 @@ const GameLoad = () => {
         <CustomButton
           title="Choose Battleground"
           handleClick={() => navigate("/battleground")}
-          restStyles="mt-5"
+          restStyles="mt-6"
         />
       </div>
 
       <div className={`flex-1 ${styles.flexCenter} flex-col`}>
         <h1 className={`${styles.headText} text-center`}>
-          Waiting for a <br /> worthy component...
+          Waiting for a <br /> worthy opponent...
         </h1>
         <p className={styles.gameLoadText}>
-          Protip: While you're waiting, choose your preferred battleground
+          Protip: while you're waiting, choose your preferred battleground
         </p>
 
         <div className={styles.gameLoadPlayersBox}>
-          {/* Player01 */}
           <div className={`${styles.flexCenter} flex-col`}>
-            <img
-              src={player01}
-              alt="player01"
-              className={styles.gameLoadPlayerImg}
-            />
+            <img src={player01} className={styles.gameLoadPlayerImg} />
             <p className={styles.gameLoadPlayerText}>
               {walletAddress.slice(0, 30)}
             </p>
           </div>
+
           <h2 className={styles.gameLoadVS}>Vs</h2>
-          {/* Unknown Player */}
+
           <div className={`${styles.flexCenter} flex-col`}>
-            <img
-              src={player02}
-              alt="player02"
-              className={styles.gameLoadPlayerImg}
-            />
-            <p className={styles.gameLoadPlayerText}>????????????????</p>
+            <img src={player02} className={styles.gameLoadPlayerImg} />
+            <p className={styles.gameLoadPlayerText}>??????????</p>
           </div>
+        </div>
+
+        <div className="mt-10">
+          <p className={`${styles.infoText} text-center mb-5`}>OR</p>
+
+          <CustomButton
+            title="Join other battles"
+            handleClick={() => navigate("/join-battle")}
+          />
         </div>
       </div>
     </div>
