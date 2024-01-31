33b3683b6a7fa8d7d5a67d550d5b4960e7047f07
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-31 12:10:46 -0600

    feat(Card): little component refactor

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,39 +5,36 @@ import styles from "../styles";
 import { allCards } from "../assets";
 
 const generateRandomCardImage = () =>
-  allCards[Math.floor(Math.random() * allCards.length - 1)];
+  allCards[Math.floor(Math.random() * (allCards.length - 1))];
 
 const img1 = generateRandomCardImage();
 const img2 = generateRandomCardImage();
 
-const Card = ({ card, title, restStyles, cardRef, playerTwo }) => {
-  return (
-    <Tilt>
-      <div ref={cardRef} className={`${styles.cardContainer} ${restStyles}`}>
-        <img
-          src={playerTwo ? img2 : img1}
-          alt="card"
-          className={styles.cardImg}
-        />
+const Card = ({ card, title, restStyles, cardRef, playerTwo }) => (
+  <Tilt>
+    <div ref={cardRef} className={`${styles.cardContainer} ${restStyles}`}>
+      <img
+        src={playerTwo ? img2 : img1}
+        alt="ace_card"
+        className={styles.cardImg}
+      />
 
-        <div
-          className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}
-        >
-          <p className={`${styles.cardPoint} text-yellow-400`}>{card.att}</p>
-        </div>
-
-        <div
-          className={`${styles.cardPointContainer} sm:right-[14.2%] right-[15%] ${styles.flexCenter}`}
-        >
-          <p className={`${styles.cardPoint} text-red-500`}>{card.def}</p>
-        </div>
-
-        <div className={`${styles.cardTextContainer} ${styles.flexCenter}`}>
-          <p className={styles.cardText}>{title}</p>
-        </div>
+      <div
+        className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}
+      >
+        <p className={`${styles.cardPoint} text-yellow-400`}>{card.att}</p>
       </div>
-    </Tilt>
-  );
-};
+      <div
+        className={`${styles.cardPointContainer} sm:right-[14.2%] right-[15%] ${styles.flexCenter}`}
+      >
+        <p className={`${styles.cardPoint} text-red-700`}>{card.def}</p>
+      </div>
+
+      <div className={`${styles.cardTextContainer} ${styles.flexCenter}`}>
+        <p className={styles.cardText}>{title}</p>
+      </div>
+    </div>
+  </Tilt>
+);
 
 export default Card;
