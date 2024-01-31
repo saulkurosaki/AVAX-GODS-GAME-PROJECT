43eaff615512dc68b01d9ff0cf049822d9f93cde
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-31 12:11:34 -0600

    feat(CustomButton): little component refactor

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 import styles from "../styles";
 
-const CustomButton = ({ title, handleClick, restStyles }) => {
-  return (
-    <button
-      type="button"
-      className={`${styles.btn} ${restStyles}`}
-      onClick={handleClick}
-    >
-      {title}
-    </button>
-  );
-};
+const CustomButton = ({ title, handleClick, restStyles }) => (
+  <button
+    type="button"
+    className={`${styles.btn} ${restStyles}`}
+    onClick={handleClick}
+  >
+    {title}
+  </button>
+);
 
 export default CustomButton;
