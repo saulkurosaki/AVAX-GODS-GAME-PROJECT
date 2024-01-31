64fa2dfd0627854475eba146429a8984bb57633b
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-31 12:19:46 -0600

    feat(OnboardModal): little component refactor

--- client/src/components/OnboardModal.jsx
+++ client/src/components/OnboardModal.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
-import Modal from 'react-modal';
+/* eslint-disable react/jsx-no-bind */
+import { useState, useEffect } from "react";
+import Modal from "react-modal";
 
-import styles from '../styles';
-import { CustomButton } from '.';
-import { useGlobalContext } from '../context';
-import { GetParams, SwitchNetwork } from '../utils/onboard.js';
+import styles from "../styles";
+import CustomButton from "./CustomButton";
+import { useGlobalContext } from "../context";
+import { GetParams, SwitchNetwork } from "../utils/onboard.js";
 
 const OnboardModal = () => {
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -20,11 +21,11 @@ const OnboardModal = () => {
   useEffect(() => {
     resetParams();
 
-    window?.ethereum?.on('chainChanged', () => {
+    window?.ethereum?.on("chainChanged", () => {
       resetParams();
     });
 
-    window?.ethereum?.on('accountsChanged', () => {
+    window?.ethereum?.on("accountsChanged", () => {
       resetParams();
     });
   }, []);
@@ -39,7 +40,7 @@ const OnboardModal = () => {
             </p>
             <CustomButton
               title="Download Core"
-              handleClick={() => window.open('https://core.app/', '_blank')}
+              handleClick={() => window.open("https://core.app/", "_blank")}
             />
           </>
         );
@@ -75,7 +76,9 @@ const OnboardModal = () => {
             </p>
             <CustomButton
               title="Grab some test tokens"
-              handleClick={() => window.open('https://faucet.avax.network/', '_blank')}
+              handleClick={() =>
+                window.open("https://faucet.avax.network/", "_blank")
+              }
             />
           </>
         );
