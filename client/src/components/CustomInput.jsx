1ce09b576a51b9dafe5077c9ecc0c2a7af4c1b89
Saul Paredes <saulkurosaki13@gmail.com>
2024-01-31 12:12:13 -0600

    feat(CustomInput): little component refactor

--- client/src/components/CustomInput.jsx
+++ client/src/components/CustomInput.jsx
@@ -4,24 +4,22 @@ import styles from "../styles";
 
 const regex = /^[A-Za-z0-9]+$/;
 
-const CustomInput = ({ label, placeholder, value, handleValueChange }) => {
-  return (
-    <>
-      <label htmlFor="name" className={styles.label}>
-        {label}
-      </label>
-      <input
-        type="text"
-        placeholder={placeholder}
-        value={value}
-        onChange={(e) => {
-          if (e.target.value === "" || regex.test(e.target.value))
-            handleValueChange(e.target.value);
-        }}
-        className={styles.input}
-      />
-    </>
-  );
-};
+const CustomInput = ({ label, placeHolder, value, handleValueChange }) => (
+  <>
+    <label htmlFor="name" className={styles.label}>
+      {label}
+    </label>
+    <input
+      type="text"
+      placeholder={placeHolder}
+      value={value}
+      onChange={(e) => {
+        if (e.target.value === "" || regex.test(e.target.value))
+          handleValueChange(e.target.value);
+      }}
+      className={styles.input}
+    />
+  </>
+);
 
 export default CustomInput;
